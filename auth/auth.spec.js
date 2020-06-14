753cbdb79a5f583912d421095c1c8d8ea2bfec3e
Ewa Czech <ewa.czech@hotmail.com>
2020-06-14 17:12:58 +0200

    organize test files

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -2,6 +2,11 @@ const db = require('../database/dbConfig.js');
 const Users = require('../users/users-model.js');
 const server = require('../api/server.js');
 const request = require('supertest');
+const router = require('express').Router();
+
+beforeEach(async () => {
+	await db('users').truncate();
+});
 
 describe('checks authentication', () => {
 	describe('test post /register', () => {
@@ -20,6 +25,14 @@ describe('checks authentication', () => {
 			user = await Users.add({ username: 'helen', password: 12345 });
 			expect(user.username).toEqual('helen');
 		});
+
+		it('should return 201 Created status', async () => {
+			const response = await request(server)
+				.post('/api/auth/register')
+				.send({ username: 'ewa', password: '12345' });
+
+			expect(response.status).toBe(201);
+		});
 	});
 
 	describe('test post /login', () => {
@@ -30,15 +43,24 @@ describe('checks authentication', () => {
 			expect(user.username).toBe('ewa');
 		});
 
-		it('should return a welcome message to the user after its successful login', async () => {
+		it('should return user after login', async () => {
 			await Users.add({ username: 'ewa', password: 12345 });
 			const user = await Users.findBy({ username: 'ewa' });
-
 			expect(user).toHaveLength(1);
 		});
-	});
-});
 
-beforeEach(async () => {
-	await db('users').truncate();
+		it('should return a welcome message to the user after its successful login', async () => {
+			const register = await request(server)
+				.post('/api/auth/register')
+				.send({ username: 'ewa', password: '12345' });
+
+			const login = await request(server)
+				.post('/api/auth/login')
+				.send({ username: 'ewa', password: '12345' });
+
+			const welcomeText = JSON.parse(login.text);
+
+			expect(welcomeText).toEqual({ message: 'Welcome ewa !' });
+		});
+	});
 });
