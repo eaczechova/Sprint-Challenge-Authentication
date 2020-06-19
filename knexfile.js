7221784c5e51304d188f2358a7972a72231b0f9b
Ewa Czech <ewa.czech@hotmail.com>
2020-06-19 06:02:14 +0200

    add changes for deployment on Heroku

--- knexfile.js
+++ knexfile.js
@@ -27,4 +27,15 @@ module.exports = {
 			directory: './database/seeds',
 		},
 	},
+
+	production: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
+		migrations: {
+			directory: './database/migrations',
+		},
+		seeds: {
+			directory: './database/seeds',
+		},
+	},
 };
